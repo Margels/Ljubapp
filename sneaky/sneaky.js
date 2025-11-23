7247172d417ed15bcc080e5b8885e16c6b3ae70b
Margels <89708428+Margels@users.noreply.github.com>
2025-11-23 21:55:28 +0100

    Update sneaky.js

--- sneaky/sneaky.js
+++ sneaky/sneaky.js
@@ -16,7 +16,7 @@ const container = document.getElementById("game-container");
 
 // --- Page structure ---
 const title = document.createElement("h2");
-title.textContent = "Sneaky peeky 📸 V2";
+title.textContent = "Sneaky peeky 📸 V3";
 container.appendChild(title);
 
 const description = document.createElement("p");
@@ -33,7 +33,7 @@ timerLabel.style.fontSize = "1.2rem";
 container.appendChild(timerLabel);
 
 // 27 Nov 2025, 11:00 Italy time (UTC+1)
-const gameStart = new Date("2025-11-23T16:52:00Z").getTime(); // UTC + 2
+const gameStart = new Date("2025-11-23T17:00:00Z").getTime(); // UTC + 2
 const gameDuration = 4 * 60 * 60 * 1000; // 4 hours
 
 function updateTimer() {
